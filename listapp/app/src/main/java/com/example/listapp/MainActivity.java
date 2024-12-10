faa56a47c3cd7c4629db034ed15c565735f4d988
Vladislav <77934904+TommyEverlie@users.noreply.github.com>
2024-12-10 18:10:14 +0200

    laborator nr 7

---
+++ listapp/app/src/main/java/com/example/listapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.listapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<State> states = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // начальная инициализация списка
+        setInitialData();
+        RecyclerView recyclerView = findViewById(R.id.list);
+        // определяем слушателя нажатия элемента в списке
+        StateAdapter.OnStateClickListener stateClickListener = (state, position) -> Toast.makeText(getApplicationContext(), "Был выбран пункт " + state.getName(),
+                Toast.LENGTH_SHORT).show();
+        // создаем адаптер
+        StateAdapter adapter = new StateAdapter(this, states, stateClickListener);
+        // устанавливаем для списка адаптер
+        recyclerView.setAdapter(adapter);
+    }
+    private void setInitialData(){
+
+        states.add(new State ("Бразилия", "Бразилиа", R.drawable.brazilia));
+        states.add(new State ("Аргентина", "Буэнос-Айрес", R.drawable.argentina));
+        states.add(new State ("Колумбия", "Богота", R.drawable.columbia));
+        states.add(new State ("Уругвай", "Монтевидео", R.drawable.uruguai));
+        states.add(new State ("Чили", "Сантьяго", R.drawable.chile));
+    }
+}
